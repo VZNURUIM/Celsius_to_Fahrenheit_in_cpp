34d63377da82e75e04a576f3214ceadb52172253
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-11-29 00:45:38 -0300

    Create Celsius_to_Fahrenheit_in_cpp.cpp

---
+++ Celsius_to_Fahrenheit_in_cpp.cpp
@@ -0,0 +1,17 @@
+/*Create a program that converts units such as temperature (Celsius to Fahrenheit)*/
+
+#include <iostream>
+
+using namespace std;
+
+double celsius(double a) {
+	cout << "Enter the temperature in Celsius : ";
+	cin >> a;
+
+	return a * 1.8 + 32.0;
+}
+
+int main() {
+	double num{};
+	cout << "Fahrenheit: " << celsius(num);
+}
